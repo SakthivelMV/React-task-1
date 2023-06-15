6ecd0813d7761fd1d900c7e5dcaa808c3c26a6f8
sakthivelm554@gmail.com <sakthivelm554@gmail.com>
2023-06-15 20:54:39 +0530

    initial commit

---
+++ src/Section.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Section(){
+    return(
+        <section className="pricing py-5">
+  <div className="container">
+    <div className="row"></div>
+    </div>
+    </section>
+    )
+}
+export default Section
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+<App/>
+</div></div>
+</section>
 );
 
 // If you want to start measuring performance in your app, pass a function
